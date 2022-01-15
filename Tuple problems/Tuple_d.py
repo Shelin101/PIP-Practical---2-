7b06691cd9f1e0ca7f976c41a8dfc542c65e8e27
Shelin <89510061+Shelin101@users.noreply.github.com>
2022-01-15 13:04:12 +0530

    Update Tuple_d.py

--- Tuple problems/Tuple_d.py
+++ Tuple problems/Tuple_d.py
@@ -1,6 +1,6 @@
 '''Student name : Shelin Vankawala 
 Student Id : 
-Practical 2 - Dictionary 
+Practical 2 - Tuple
 Aim d : Write a Python program to convert a tuple to a string.'''
 
 tupled = ('M', 'i', 's', 's', 'i', 's', 's', 'i', 'p', 'p', 'i')
