c85248425a9dae8d14dbf0022a77373c4cc747e9
Shelin <89510061+Shelin101@users.noreply.github.com>
2022-01-15 13:02:41 +0530

    Update Set_a.py

--- Set problems/Set_a.py
+++ Set problems/Set_a.py
@@ -2,7 +2,7 @@
 Student Id : 20CS101
 Practical 2 - Set
 Aim a : Write a Python program to add member(s) in a set and clear a set.'''
-
+'''https://github.com/Shelin101/PIP-Practical---2-'''
 set1 = {'Rohit', 'Siya', 'Jessica', 'Aditi'}
 set1.add('Darshiv')
 print(set1)
