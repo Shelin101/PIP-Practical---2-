045a9b0782aa2c14bc4ff0f13820aaf99673558e
Shelin <89510061+Shelin101@users.noreply.github.com>
2022-01-15 13:03:15 +0530

    Update Tuple_a.py

--- Tuple problems/Tuple_a.py
+++ Tuple problems/Tuple_a.py
@@ -1,8 +1,8 @@
 '''Student name : Shelin Vankawala 
 Student Id : 
-Practical 2 - Dictionary 
+Practical 2 - Tuple
 Aim a : Write a Python program to create a tuple with different data types.'''
-
+'''https://github.com/Shelin101/PIP-Practical---2-'''
 Tuple1 = ('int', 'bool', 'float')
 Tuple2 = (12, 3, 8)
 Tuple3 = (12.456, 0.1)
