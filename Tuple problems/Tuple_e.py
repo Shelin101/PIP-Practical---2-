a0b2a53ebb7f779c69e7baa0233e9f12ae51e2f6
Shelin <89510061+Shelin101@users.noreply.github.com>
2022-01-15 13:04:32 +0530

    Update Tuple_e.py

--- Tuple problems/Tuple_e.py
+++ Tuple problems/Tuple_e.py
@@ -1,6 +1,6 @@
 '''Student name : Shelin Vankawala 
 Student Id : 
-Practical 2 - Dictionary 
+Practical 2 - Tuple
 Aim e : Write a Python program to find the length of a tuple.'''
 
 tuplee = ("Riya", "Mohit", "Akansha")
