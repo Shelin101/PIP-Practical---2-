4698f769a8b9afcd6d75223f68dbb536e0b9263f
Shelin <89510061+Shelin101@users.noreply.github.com>
2022-01-15 13:02:14 +0530

    Update Dictionary_a.py

--- Dictionary problems/Dictionary_a.py
+++ Dictionary problems/Dictionary_a.py
@@ -2,6 +2,7 @@
 Student Id : 20CS101
 Practical 2 - Dictionary 
 Aim a : Write a Python script to check whether a given key already exists in a dictionary.'''
+'''https://github.com/Shelin101/PIP-Practical---2-'''
 Person = {'Rohit': '1', 'Siya': '2',
           'Jessica': '3', 'Aditi': '4', 'Darshiv': '5'}
 
